31fd1e7e29781510e97a6ebca65c90e73a99e0d8
Saransh Seth <saranshseth93@gmail.com>
2023-01-28 14:06:30 +1100

    Pushing airbnb-clone to github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key: "pk.eyJ1Ijoic2FyYW5zaHNldGg5MyIsImEiOiJjbGRmY2ZidncwaGdlM3FtcGFtcTRyM3RsIn0.KALqqlJLITSxNEdiBbRnAQ",
+  },
+};
